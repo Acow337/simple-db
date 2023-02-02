2fc838238123800dcfba130bd629297fefe4277e
Acow337 <yzjin2001@qq.com>
2023-02-02 14:07:26 +0800

    implement Filter

--- src/java/simpledb/execution/Filter.java
+++ src/java/simpledb/execution/Filter.java
@@ -5,6 +5,9 @@ import simpledb.storage.Tuple;
 import simpledb.storage.TupleDesc;
 import simpledb.transaction.TransactionAbortedException;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 /**
@@ -14,6 +17,12 @@ public class Filter extends Operator {
 
     private static final long serialVersionUID = 1L;
 
+    private Predicate predicate;
+    private OpIterator child;
+    private Iterator<Tuple> it;
+    private final TupleDesc td;
+    private final List<Tuple> childTups = new ArrayList<>();
+
     /**
      * Constructor accepts a predicate to apply and a child operator to read
      * tuples to filter from.
@@ -22,30 +31,35 @@ public class Filter extends Operator {
      * @param child The child operator
      */
     public Filter(Predicate p, OpIterator child) {
-        // TODO: some code goes here
+        predicate = p;
+        this.child = child;
+        td = child.getTupleDesc();
     }
 
     public Predicate getPredicate() {
-        // TODO: some code goes here
-        return null;
+        return predicate;
     }
 
     public TupleDesc getTupleDesc() {
-        // TODO: some code goes here
         return null;
     }
 
-    public void open() throws DbException, NoSuchElementException,
-            TransactionAbortedException {
-        // TODO: some code goes here
+    public void open() throws DbException, NoSuchElementException, TransactionAbortedException {
+        child.open();
+        // load all the tuples in a collection
+        while (child.hasNext())
+            childTups.add(child.next());
+        it = childTups.iterator();
+        super.open();
     }
 
     public void close() {
-        // TODO: some code goes here
+        super.close();
+        it = null;
     }
 
     public void rewind() throws DbException, TransactionAbortedException {
-        // TODO: some code goes here
+        it = childTups.iterator();
     }
 
     /**
@@ -54,24 +68,26 @@ public class Filter extends Operator {
      * pass the predicate (i.e. for which the Predicate.filter() returns true.)
      *
      * @return The next tuple that passes the filter, or null if there are no
-     *         more tuples
+     * more tuples
      * @see Predicate#filter
      */
-    protected Tuple fetchNext() throws NoSuchElementException,
-            TransactionAbortedException, DbException {
-        // TODO: some code goes here
-        return null;
+    protected Tuple fetchNext() throws NoSuchElementException, TransactionAbortedException, DbException {
+        if (it != null && it.hasNext()) {
+            return it.next();
+        } else
+            return null;
     }
 
     @Override
     public OpIterator[] getChildren() {
-        // TODO: some code goes here
-        return null;
+        return new OpIterator[]{this.child};
     }
 
     @Override
     public void setChildren(OpIterator[] children) {
-        // TODO: some code goes here
+        if (this.child != children[0]) {
+            this.child = children[0];
+        }
     }
 
 }
