9dfff6c6f6ad87dd4629f5c73193a5e8cac7885c
Acow337 <yzjin2001@qq.com>
2023-02-21 15:15:28 +0800

    update LockManager

--- src/java/simpledb/common/LockManager.java
+++ src/java/simpledb/common/LockManager.java
@@ -1,46 +1,55 @@
 package simpledb.common;
 
 import simpledb.storage.PageId;
+import simpledb.transaction.TransactionId;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 // refer to bustub https://github.com/cmu-db/bustub
 // page-level lock
 public class LockManager {
     enum LockMode {
-        SHARED,
-        EXCLUSIVE;
+        SHARED, EXCLUSIVE;
     }
 
     class LockRequest {
-        public int tid;
+        public TransactionId tid;
         public LockMode lockMode;
         public boolean granted = false;
 
-        public LockRequest(int tid, LockMode lockMode, PageId pageId, boolean granted) {
+        public LockRequest(TransactionId tid, LockMode lockMode, PageId pageId) {
             this.tid = tid;
             this.lockMode = lockMode;
-            this.granted = granted;
         }
     }
 
     class LockRequestQueue {
-        public BlockingQueue<LockRequest> requestQueue;
+        public BlockingQueue<LockRequest> queue;
         // coordination
         public Lock latch;
         public Condition condition;
-        public int upgradingTid;
+        public TransactionId upgradingTid;
 
-        public LockRequestQueue(BlockingQueue<LockRequest> requestQueue, Lock latch, Condition condition, int upgradingTid) {
-            this.requestQueue = requestQueue;
-            this.latch = latch;
-            this.condition = condition;
-            this.upgradingTid = upgradingTid;
+        public LockRequestQueue() {
+            queue = new SynchronousQueue<>();
+            latch = new ReentrantLock();
+            condition = latch.newCondition();
+        }
+
+        public boolean offer(LockRequest request) {
+            return queue.offer(request);
+        }
+
+        public LockRequest poll() {
+            return queue.poll();
         }
     }
 
@@ -56,12 +65,95 @@ public class LockManager {
         cycleDetectionThread = new Thread();
     }
 
-    public void lockPage() {
+    public void lockPage(TransactionId tid, Permissions perm, PageId pageId) throws DeadlockException {
+        if (!pageLockMap.containsKey(pageId)) pageLockMap.put(pageId, new LockRequestQueue());
+        LockMode mode = null;
+        LockMode formerMode = null;
+        LockRequest formerRequest = null;
+        int lockNum = 0;
+        boolean isOtherHasLock = false;
+        if (perm == Permissions.READ_ONLY) {
+            mode = LockMode.SHARED;
+        } else if (perm == Permissions.READ_WRITE) {
+            mode = LockMode.EXCLUSIVE;
+        }
 
+        LockRequest newRequest = new LockRequest(tid, mode, pageId);
+        LockRequestQueue requestQueue = pageLockMap.get(pageId);
+
+        // if no lock on the page, lock literally
+        if (requestQueue.queue.isEmpty()) {
+            newRequest.granted = true;
+            requestQueue.offer(newRequest);
+            return;
+        }
+
+        // check if the txn already has lock
+        for (LockRequest request : requestQueue.queue) {
+            lockNum++;
+            if (request.tid == tid) {
+                formerRequest = request;
+            } else {
+                isOtherHasLock = true;
+            }
+        }
+
+        //TODO if the txn already has lock on the page
+        if (formerRequest != null) {
+            // the txn already has lock
+            if (formerMode == LockMode.SHARED) {
+                if (mode == LockMode.SHARED) {
+                    return;
+                } else if (mode == LockMode.EXCLUSIVE) {
+                    if (isOtherHasLock) {
+                        // throw exception because deadlock may happen (upgrade failure)
+                        throw new DeadlockException();
+                    } else {
+                        // upgrade the lock
+                        formerRequest.lockMode = LockMode.EXCLUSIVE;
+                        return;
+                    }
+                }
+            } else if (formerMode == LockMode.EXCLUSIVE) {
+                return;
+            }
+        } else {
+            if (lockNum > 1) {
+                if (newRequest.lockMode == LockMode.SHARED) {
+                    requestQueue.offer(newRequest);
+                    return;
+                } else {
+                    requestQueue.condition.awaitUninterruptibly();
+                    lockPage(tid, perm, pageId);
+                }
+            } else if (lockNum == 1) {
+                LockRequest other = requestQueue.queue.peek();
+                if (other.lockMode == LockMode.EXCLUSIVE) {
+                    requestQueue.condition.awaitUninterruptibly();
+                    lockPage(tid, perm, pageId);
+                } else if (other.lockMode == LockMode.SHARED) {
+                    if (mode == LockMode.SHARED) {
+                        requestQueue.offer(newRequest);
+                        return;
+                    } else if (mode == LockMode.EXCLUSIVE) {
+                        requestQueue.condition.awaitUninterruptibly();
+                        lockPage(tid, perm, pageId);
+                    }
+                }
+            }
+        }
     }
 
-    public void unLockPage() {
-
+    public void unLockPage(TransactionId tid, PageId pageId) {
+        LockRequestQueue requestQueue = pageLockMap.get(pageId);
+        LockRequest removeRequest = null;
+        for (LockRequest request : requestQueue.queue) {
+            if (tid == request.tid) {
+                removeRequest = request;
+            }
+        }
+        requestQueue.queue.remove(removeRequest);
+        requestQueue.condition.signalAll();
     }
 
 }
